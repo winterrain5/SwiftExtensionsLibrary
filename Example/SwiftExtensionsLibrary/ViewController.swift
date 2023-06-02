086d1681c00f09b1e2cac71c64f6c9a365fd53e7
winterrain5 <913419042@qq/com>
2023-06-02 09:37:34 +0800

    Initial commit

---
+++ Example/SwiftExtensionsLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftExtensionsLibrary
+//
+//  Created by winterrain5 on 06/02/2023.
+//  Copyright (c) 2023 winterrain5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
